a6704ed37b95bedeea7eb48532105a233d1e980c
Yuri Pimenov <up47@bk.ru>
2017-05-22 21:07:42 +0300

    22.05.17-Cx

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,34 @@
 /* GET home page */
 module.exports.homelist = function(req, res){
-  res.render('locations-list', { title: 'Home' });
-};
+  res.render('locations-list', { 
+        title: 'Loc8r - find a place to work with wifi',
+        string: 'Глядя на Wi-Fi и сиденье? Loc8r поможет вам найти места, чтобы работать, когда и о. Может быть, с кофе, пирожное или пинту? Пусть Loc8r поможет вам найти место, которое вы ищете for.elp вам найти место, которое вы ищете.',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Найти места для работы с Wi-Fi рядом с вами!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Степашка',
+            address: 'Санкт-Петербург ул. Гагарина дом 5',
+            rating: 3,
+            facilities: ['Напитки', 'Горячия еда', 'Премиум Wi-Fi'],
+            distance: '150m'
+        }, {
+            name: 'У Хрюшки и степашки',
+            address: 'Санкт-Петербург ул. Вепра дом 1050',
+            rating: 2,
+            facilities: ['Напитки', 'Горячия еда', 'Премиум Wi-Fi'],
+            distance: '200m'
+        }, {
+            name: 'Золотая Устрица',
+            address: 'Санкт-Петербург ул. Савушкина дом 105',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
+ };
 /* GET home page */
 module.exports.locationsInfo = function(req, res){
   res.render('location-info', { title: 'locations info' });
