978081a0a9339726c5213476c58c29ab1391797a
Yuri Pimenov <up47@bk.ru>
2017-05-24 14:39:33 +0300

    24.05 sx2

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,9 +31,58 @@ module.exports.homelist = function(req, res){
  };
 /* GET home page */
 module.exports.locationsInfo = function(req, res){
-  res.render('location-info', { title: 'locations info' });
+  res.render('location-info', { 
+  	title: 'Золотая Устрица',
+  	  pageHeader: {
+            title: 'Золотая Устрица'
+        },
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Золотая Устрица',
+            address: 'Санкт-Петербург ул. Савушкина дом 105',
+            rating: 2,
+            facilities: ['Hot drinks', 'Premium wifi'],
+            coords: {
+                lat: 37.4425930,
+                lng: -89.0725160
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    }); 
 };
 /* GET home page */
-module.exports.addReview = function(req, res){
-  res.render('location-review-form', { title: 'Add review' });
+module.exports.addReview = function(req, res) {
+    res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: {
+            title: 'Review Starcups'
+        }
+    });
 };
